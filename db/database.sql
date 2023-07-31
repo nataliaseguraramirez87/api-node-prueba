c92cb60f7e826e00100736c2c0704432808b4dc3
Natalia Segura Ramírez <nsegura@easynet.com.co>
2023-07-31 09:35:06 -0500

    Corrección de bases de datos

--- db/database.sql
+++ db/database.sql
@@ -8,16 +8,14 @@ CREATE TABLE product (
   description VARCHAR(255) DEFAULT NULL,
   valor INT(11) DEFAULT NULL, 
   imageURL varchar(255) NOT NULL,
-  createdAt datetime NOT NULL,
-  updatedAt datetime NOT NULL,
   PRIMARY KEY(id)
 );
 
 DESCRIBE product;
 
 INSERT INTO product values 
-  (1, 'Producto 1', 'Descripción de los productos', 20000, 'https://conceptoabc.com/wp-content/uploads/2021/09/Producto.jpg','2023-07-31 02:27:09', '2023-07-31 02:27:09'),
-  (2, 'Producto 2', 'Descripción de los productos', 40000, 'https://conceptoabc.com/wp-content/uploads/2021/09/Producto.jpg','2023-07-31 02:27:09', '2023-07-31 02:27:09'),
-  (3, 'Producto 3', 'Descripción de los productos', 50000, 'https://conceptoabc.com/wp-content/uploads/2021/09/Producto.jpg','2023-07-31 02:27:09', '2023-07-31 02:27:09');
+  (1, 'Producto 1', 'Descripción de los productos', 20000, 'https://conceptoabc.com/wp-content/uploads/2021/09/Producto.jpg'),
+  (2, 'Producto 2', 'Descripción de los productos', 40000, 'https://conceptoabc.com/wp-content/uploads/2021/09/Producto.jpg'),
+  (3, 'Producto 3', 'Descripción de los productos', 50000, 'https://conceptoabc.com/wp-content/uploads/2021/09/Producto.jpg');
 
 SELECT * FROM product;
